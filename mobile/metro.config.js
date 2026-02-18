2ebf455b4ea1fa4cb50e56eb2a01d2e597821fde
Romano98-dev <luca.caterini@outlook.it>
2026-02-18 15:35:18 +0100

    g

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -2,15 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Risolvi i moduli Node non supportati in React Native
-config.resolver.extraNodeModules = {
-    http2: false,
-    zlib: false,
-    fs: false,
-    net: false,
-    tls: false,
-    child_process: false,
-    crypto: false,
-};
-
 module.exports = config;
